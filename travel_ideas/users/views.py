2cc1547bba2bc60433e7067b54a62ffc1ddedb9d
Maria Shatova <marialshy@gmail.com>
2024-08-14 19:06:57 +0300

    created user registration form (custom form), available only by direct link "/register"- view register

--- travel_ideas/users/views.py
+++ travel_ideas/users/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import get_user_model, login
+from django.contrib import messages
+from .forms import UserRegistrationForm
 # Create your views here.
+
+
+def register(request):
+    if request.user.is_authenticated:
+        return redirect('/')
+    
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, f'Created new account: {user.username}')
+            return redirect('/')
+        
+        else:
+            for error in list(form.errors.values()):
+                messages.error(request, error)
+                print(request, error)
+    else:
+        form = UserRegistrationForm()
+
+    return render(
+        request,
+        "users/registration.html",
+        context={'form': form}
+    )
